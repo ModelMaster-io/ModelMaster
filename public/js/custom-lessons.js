7240a5877ff6271492fde15818c0065316ef62c0
modelmasterio <58180596+modelmasterio@users.noreply.github.com>
2020-02-18 09:11:45 -0500

    Updated Load
    Corrected Loading Functionality

--- public/js/custom-lessons.js
+++ public/js/custom-lessons.js
@@ -40,43 +40,6 @@ jQuery(document).ready(function () {
 
 	          	var screen = response['screen'];
 	          	var step = response['step'];
-	          	
-	          	if(screen == 1 && step == 6){
-	          		screen = 2;
-	          		step = 1;
-	          	} else if (screen == 2 && step == 5){
-	          		screen = 3;
-	          		step = 1;
-	          	} else if (screen == 3 && step == 6){
-	          		screen = 4;
-	          		step = 1;
-	          	} else if (screen == 4 && step == 7){
-	          		screen = 5;
-	          		step = 1;
-	          	} else if (screen == 5 && step == 9){
-	          		screen = 6;
-	          		step = 1;
-	          	} else if (screen == 6 && step == 5){
-	          		screen = 7;
-	          		step = 1;
-	          	} else if (screen == 7 && step == 9){
-	          		screen = 8;
-	          		step = 1;
-	          	} else if (screen == 8 && step == 7){
-	          		screen = 9;
-	          		step = 1;
-	          	} else if (screen == 9 && step == 5){
-	          		screen = 10;
-	          		step = 1;
-	          	} else if (screen == 10 && step == 8){
-	          		screen = 11;
-	          		step = 1;
-	          	} else if (screen == 11 && step == 2){
-	          		screen = 1;
-	          		step = 1;
-	          	} else {
-	          		step += 1;
-	          	}
 
 	          	jQuery('.spread_steps_clk li a').removeClass('active');
 	          	jQuery(".spread_steps_clk li:nth-child("+screen+") a").addClass('active');
@@ -86,6 +49,9 @@ jQuery(document).ready(function () {
 
 	          	jQuery("#step"+screen+" .spread_sub_steps_clk li a").removeClass('active');
 	          	jQuery("#step"+screen+" .spread_sub_steps_clk li:nth-child("+step+") a").addClass('active');
+				
+				jQuery('.lesson-contant-left-tab-contant #'+step).show();
+				jQuery(".lcltc1-mm").getNiceScroll().show().onResize();
 
 	          }
 
